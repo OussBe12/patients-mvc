a4ed684a6b1bc6fe641d723ab9e6124d5a0736d2
Oussama-benc <74989020+Oussama-benc@users.noreply.github.com>
2022-04-24 02:12:16 +0100

    Commit part 1 and part 2 v2

--- src/main/java/ma/emsi/patientsmvc/PatientsMvcApplication.java
+++ src/main/java/ma/emsi/patientsmvc/PatientsMvcApplication.java
@@ -20,7 +20,7 @@ CommandLineRunner commandLineRunner(PatientRepository patientRepository){
         return args ->{
             patientRepository.save(new Patient(null,"Oussama1",new Date(),false,122));
             patientRepository.save(new Patient(null,"Ali",new Date(),true,321));
-            patientRepository.save(new Patient(null,"testcommit",new Date(),true,165));
+            patientRepository.save(new Patient(null,"Said",new Date(),true,165));
             patientRepository.save(new Patient(null,"Oussama2",new Date(),false,132));
             patientRepository.findAll().forEach(p->{
                 System.out.println(p.getNom());
